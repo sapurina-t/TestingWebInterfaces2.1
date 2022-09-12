75013988396187f86ba97fcbf951d35b83c5ff6f
taya sapurina <sapurina0t@gmail.com>
2022-09-13 01:02:10 +0500

    add selenium

---
+++ src/test/java/ru/netology/web/CardOrderingTest.java
@@ -0,0 +1,41 @@
+package ru.netology.web;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class CardOrderingTest {
+    WebDriver driver;
+
+    @BeforeAll
+    static void setupAll() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setup() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999/");
+    }
+
+    @AfterEach
+    void teardown() {
+        driver.quit();
+        driver=null;
+    }
+
+    @Test
+    void test() {
+        // Your test logic here
+    }
+}
