784f79c45daf88c995a566020997033b4448cf76
taya sapurina <sapurina0t@gmail.com>
2022-09-14 19:29:13 +0500

    test Card Ordering

--- src/test/java/ru/netology/web/CardOrderingTest.java
+++ src/test/java/ru/netology/web/CardOrderingTest.java
@@ -4,14 +4,20 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CardOrderingTest {
-    WebDriver driver;
+   private WebDriver driver;
 
     @BeforeAll
     static void setupAll() {
@@ -25,7 +31,7 @@ public class CardOrderingTest {
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
-//        driver.get("http://localhost:9999/");
+        driver.get("http://localhost:9999/");
     }
 
     @AfterEach
@@ -35,7 +41,13 @@ public class CardOrderingTest {
     }
 
     @Test
-    void test() {
-        driver.get("http://localhost:9999/");
+    void shouldSendTheForm() {
+
+        driver.findElement(By.cssSelector("[data-test-id=\"name\"] input")).sendKeys("Имя Фамилия");
+        driver.findElement(By.cssSelector("[data-test-id=\"phone\"] input")).sendKeys("+79121234567");
+        driver.findElement(By.cssSelector(".checkbox__box")).click();
+        driver.findElement(By.cssSelector(".button__text")).click();
+        String actual = driver.findElement(By.cssSelector("[data-test-id=\"order-success\"]")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", actual.trim());
     }
 }
